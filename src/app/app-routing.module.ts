8981a273cdc8e17b9e8de881a23b8f26f1176104
Bryan13133 <52942708+Bryan13133@users.noreply.github.com>
2020-07-21 12:36:53 -0600

    Routes Creation

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,7 +2,25 @@ import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: 'general',
+    redirectTo: '/general',
+    pathMatch: 'full'
+  },
+  {
+    path: 'general',
+    component: GeneralComponent
+  },
+  {
+    path: 'country',
+    component: CountryComponent
+  },
+  {
+    path: '**',
+    component: NotFoundComponent
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
