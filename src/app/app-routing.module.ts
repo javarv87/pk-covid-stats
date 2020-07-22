c1fc550e2cfa059d3e6e09e158d285e50f057370
Bryan13133 <52942708+Bryan13133@users.noreply.github.com>
2020-07-22 08:12:54 -0600

    PKS01US03 Creation of Routes

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -4,7 +4,7 @@ import { Routes, RouterModule } from '@angular/router';
 
 const routes: Routes = [
   {
-    path: 'general',
+    path: '',
     redirectTo: '/general',
     pathMatch: 'full'
   },
