ba291a777dba2e0413f9e6b2dbb671c43e782ab3
Javier Ruiz Vázquez <java.rv87@gmail.com>
2020-07-23 18:38:29 -0500

    Modify files to reference to styles folder and components folder

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,11 +4,15 @@ import { NgModule } from '@angular/core';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { NotFoundComponent } from './components/not-found/not-found.component';
+import { HeaderComponent } from '@pk-components/header/header.component';
+import { ToggleSwtichComponent } from '@pk-components/toggle-swtich/toggle-swtich.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    NotFoundComponent
+    NotFoundComponent,
+    HeaderComponent,
+    ToggleSwtichComponent
   ],
   imports: [
     BrowserModule,
