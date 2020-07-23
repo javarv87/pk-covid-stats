61fc39642ae36a6e1a1b7138613c0e8396205732
Javier Ruiz Vázquez <java.rv87@gmail.com>
2020-07-23 18:40:51 -0500

    Create Header component and toggleswitch component

---
+++ src/app/components/toggle-swtich/toggle-swtich.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'pk-toggle-swtich',
+  templateUrl: './toggle-swtich.component.html',
+  styleUrls: ['./toggle-swtich.component.sass']
+})
+export class ToggleSwtichComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
