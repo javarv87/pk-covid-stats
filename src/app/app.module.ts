d94a711e3f7f62950d9ee6227d8e2d8be53f0aa3
RobertoRm3 <robertorm@example.com>
2020-07-24 19:50:32 -0500

    adding component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,32 +3,32 @@ import { NgModule, APP_INITIALIZER } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-<<<<<<< HEAD
+
 import { CountryPageComponentComponent } from './Component/country-page-component/country-page-component.component';
-=======
+
 import { FooterComponent } from './components/footer/footer.component';
 import { NotFoundComponent } from './components/not-found/not-found.component';
 import { HeaderComponent } from '@pk-components/header/header.component';
 import { ToggleSwtichComponent } from '@pk-components/toggle-swtich/toggle-swtich.component';
->>>>>>> dab657a959c6cf7c6a13e094e0b02cd891d917ac
+
 
 import { ThemeService } from '@pk-services/theme/theme.service';
 
 export function themeFactory(themeService: ThemeService) {
   return () => themeService.setInitialTheme();
 }
- 
+
 @NgModule({
   declarations: [
     AppComponent,
-<<<<<<< HEAD
-    CountryPageComponentComponent
-=======
+
+    CountryPageComponentComponent,
+
     NotFoundComponent,
     HeaderComponent,
     FooterComponent,
     ToggleSwtichComponent
->>>>>>> dab657a959c6cf7c6a13e094e0b02cd891d917ac
+
   ],
   imports: [
     BrowserModule,
