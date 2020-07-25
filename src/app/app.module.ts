26a785b5b7a1688756548b66af4cefeed6af4090
RobertoRm3 <robertorm@example.com>
2020-07-24 19:25:02 -0500

    inicial commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,12 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { CountryPageComponentComponent } from './Component/country-page-component/country-page-component.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    CountryPageComponentComponent
   ],
   imports: [
     BrowserModule,
