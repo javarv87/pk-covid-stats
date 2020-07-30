161449ece2909a0fe32e8125dd02b24e27e47d83
Javier Ruiz Vázquez <java.rv87@gmail.com>
2020-07-30 14:10:45 -0500

    Merge changes from PKS01US06

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,21 +4,15 @@ import { NgModule, APP_INITIALIZER } from '@angular/core';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 
-<<<<<<< HEAD
-import { CountryPageComponentComponent } from './Component/country-page-component/country-page-component.component';
+// Pages
+import { DefaultPageComponent } from '@pk-components/default-page/default-page.component';
+import { CountryPageComponent } from '@pk-components/country-page/country-page.component';
+import { NotFoundPageComponent } from '@pk-components/not-found-page/not-found-page.component';
 
-import { FooterComponent } from './components/footer/footer.component';
-import { NotFoundComponent } from './components/not-found/not-found.component';
-=======
-
-import { DefaultPageComponent } from './components/default-page/default-page.component';
-
+// Components
 import { FooterComponent } from '@pk-components/footer/footer.component';
-import { NotFoundComponent } from '@pk-components/not-found/not-found.component';
->>>>>>> 7b14a78347029eac83f4a93ec7d9eecc2eed9146
 import { HeaderComponent } from '@pk-components/header/header.component';
 import { ToggleSwtichComponent } from '@pk-components/toggle-swtich/toggle-swtich.component';
-
 
 import { ThemeService } from '@pk-services/theme/theme.service';
 
@@ -26,30 +20,15 @@ export function themeFactory(themeService: ThemeService) {
   return () => themeService.setInitialTheme();
 }
 
-<<<<<<< HEAD
-@NgModule({
-  declarations: [
-    AppComponent,
-
-    CountryPageComponentComponent,
-
-    NotFoundComponent,
-    HeaderComponent,
-    FooterComponent,
-    ToggleSwtichComponent
-
-=======
-
-
 @NgModule({
   declarations: [
     AppComponent,
     DefaultPageComponent,
-    NotFoundComponent,
+    CountryPageComponent,
+    NotFoundPageComponent,
     HeaderComponent,
-    ToggleSwtichComponent,
-    FooterComponent
->>>>>>> 7b14a78347029eac83f4a93ec7d9eecc2eed9146
+    FooterComponent,
+    ToggleSwtichComponent
   ],
   imports: [
     BrowserModule,
