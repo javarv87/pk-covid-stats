5f67ba62e22d0ab5bbf13071636443fc33ff5b71
Santiago Roman <santroma1@gmail.com>
2020-07-29 21:37:35 -0500

    Create API calls in service, plus 3 models that resemble API data

---
+++ src/app/services/main/main.service.ts
@@ -0,0 +1,48 @@
+import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Observable } from 'rxjs';
+import { Country } from "../../models/country";
+import { TimelineData } from "../../models/timeline-data";
+import { GlobalTimelineData } from "../../models/global-timeline-data";
+
+@Injectable({
+  providedIn: 'root'
+})
+export class MainService {
+
+  constructor(private http:HttpClient) { }
+
+  //Returns global deaths, recovered,cases,
+  getMainStats():Observable<any>{
+    return this.http.get<any>("http://api.coronastatistics.live/all");
+  }
+
+  getAllCountriesStats():Observable<Country[]>{
+    return this.http.get<Country[]>("http://api.coronastatistics.live/countries");
+  }
+
+  getStatsByCountry(country:string):Observable<Country>{
+    return this.http.get<Country>(`http://api.coronastatistics.live/countries/${country}`);
+  }
+
+  //This returns all the countries and data for dates, anything involving TimelineData is for the AmCharts
+  getAllCountriesTimelineData():Observable<TimelineData[]>{
+    return this.http.get<TimelineData[]>("http://api.coronastatistics.live/timeline")
+  }
+
+
+  //Returns data for the amcharts but specific to a country
+  getTimelineDataByCountry(country:string):Observable<TimelineData>{
+    return this.http.get<TimelineData>(`http://api.coronastatistics.live/timeline/${country}`);
+  }
+
+  //Global Data for AmCharts
+  getGlobalTimelineData():Observable<GlobalTimelineData>{
+    return this.http.get<GlobalTimelineData>('http://api.coronastatistics.live/timeline/global');
+  }
+
+
+
+
+
+}
