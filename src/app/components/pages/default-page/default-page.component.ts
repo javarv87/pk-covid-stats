95f3491a81c442205e714ad844e128f39cdc77e3
yogar <lalogrougon@gmail.com>
2020-08-07 23:43:22 -0500

    PayChart implement #1

--- src/app/components/pages/default-page/default-page.component.ts
+++ src/app/components/pages/default-page/default-page.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit, NgZone, AfterViewInit, OnDestroy } from '@angular/core';
-import { MainService } from '../../../services/main/main.service';
+import { MainService } from 'src/app/services/main/main.service';
+import { Subscription } from 'rxjs';
 
 import * as am4core from '@amcharts/amcharts4/core';
 import * as am4charts from '@amcharts/amcharts4/charts';
@@ -11,10 +12,12 @@ import * as am4charts from '@amcharts/amcharts4/charts';
 })
 export class DefaultPageComponent implements OnInit, AfterViewInit, OnDestroy {
 
-  private chart: am4charts.PieChart;
+  private payChart: am4charts.PieChart;
+  sub: Subscription;
 
   constructor(
-  	private zone: NgZone
+  	private zone: NgZone,
+    private mainService: MainService
   ) { }
 
   ngOnInit(): void {
@@ -22,53 +25,91 @@ export class DefaultPageComponent implements OnInit, AfterViewInit, OnDestroy {
 
   ngAfterViewInit() {
     this.zone.runOutsideAngular(() => {
-      this.loadInfectedDistribution();
+      this.loadInfectedPayChart();
     });
   }
 
-  loadInfectedDistribution() {
-    let chart = am4core.create('infected-distribution', am4charts.PieChart);
+  loadInfectedPayChart() {
+    let tempArr;
+    this.sub = this.mainService.getAllCountriesStats().subscribe(data => tempArr);
+    let auxPayChart = am4core.create("payChart", am4charts.PieChart);
+    let others = tempArr.slice(10,tempArr.length)
 
-    chart.data = [{
+    this.sortem(tempArr, "cases");
+    tempArr = tempArr.reverse();
+
+    auxPayChart.data = tempArr.slice(0,10);
+    auxPayChart.data.push({
+      country: 'Other',
+      cases: this.addCases("cases",others)
+    });
+
+    /*auxPayChart.data = [{
       "country": "Lithuania",
-      "litres": 501.9
+      "cases": 501.9
     }, {
       "country": "Czech Republic",
-      "litres": 301.9
+      "cases": 301.9
     }, {
       "country": "Ireland",
-      "litres": 201.1
+      "cases": 201.1
     }, {
       "country": "Germany",
-      "litres": 165.8
+      "cases": 165.8
     }, {
       "country": "Australia",
-      "litres": 139.9
+      "cases": 139.9
     }, {
       "country": "Austria",
-      "litres": 128.3
+      "cases": 128.3
     }, {
       "country": "UK",
-      "litres": 99
+      "cases": 99
     }, {
       "country": "Belgium",
-      "litres": 60
+      "cases": 60
     }, {
       "country": "The Netherlands",
-      "litres": 50
-    }];
+      "cases": 50
+    }];*/
 
-    let pieSeries = chart.series.push(new am4charts.PieSeries());
-    pieSeries.dataFields.value = "litres";
-    pieSeries.dataFields.category = "country";
+    let paySeries = auxPayChart.series.push(new am4charts.PieSeries());
+    paySeries.dataFields.value = "cases";
+    paySeries.dataFields.category = "country";
 
-    this.chart = chart;
+    this.payChart = auxPayChart;
+  }
+
+  addCases(category, paramArr) {
+    let result = 0;
+    for (var i = 0 ; i<paramArr.length ; i+=1) {
+      result += paramArr[i][category];
+    }
+    return result;
+  }
+
+  sortem(info, sortParam) {
+    try {
+      const forSort = sortParam;
+      info.sort((a, b) => {
+        if (a[forSort] < b[forSort]) {
+          return -1;
+        } else if (a[forSort] > b[forSort]) {
+          return 1;
+        }
+        return 0;
+      })
+    } catch (e) {
+      console.error("ERROR while sorting", e);
+      return info;
+    }
+    return info;
   }
 
   ngOnDestroy() {
     this.zone.runOutsideAngular(() => {
-      if (this.chart) {
-        this.chart.dispose();
+      if (this.payChart) {
+        this.payChart.dispose();
       }
     });
   }
