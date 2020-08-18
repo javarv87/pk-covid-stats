d04cd45026fed18c0aa1a5b0e7444c4edb93748f
Bryan13133 <52942708+Bryan13133@users.noreply.github.com>
2020-08-18 17:43:59 -0600

    PKS05US02 Country Infection Distribution by Countries

--- src/app/components/pages/country-page/country-page.component.ts
+++ src/app/components/pages/country-page/country-page.component.ts
@@ -13,6 +13,7 @@ export class CountryPageComponent implements OnInit {
 
   countryName: string;
   radarChart: am4charts.RadarChart;
+  pieChart: am4charts.PieChart;
   totalCritical:any;
   activeCases:any;
   totalDeaths:any;
@@ -282,6 +283,7 @@ export class CountryPageComponent implements OnInit {
       this.totalRecoveries=data.recovered;
       this.finishedCases= this.totalDeaths + this.totalRecoveries;
     this.createRadarChart();
+    this.createPieChart();
     }) ;
   }
   createRadarChart() {
@@ -372,4 +374,32 @@ export class CountryPageComponent implements OnInit {
     this.radarChart = chart;
   }
 
+  createPieChart() {
+    let chart = am4core.create("pieChart", am4charts.PieChart);
+    chart.data.push({
+      type: 'Recoveries',
+      number: this.totalRecoveries,
+      "color": am4core.color("#10c469")
+    });
+    chart.data.push({
+      type: 'Deaths',
+      number: this.totalDeaths,
+      "color": am4core.color("#ff5b5b")
+    });
+    chart.data.push({
+      type: 'Critical',
+      number: this.totalCritical,
+      "color": am4core.color("#f9c851")
+    });
+    let pieSeries = chart.series.push(new am4charts.PieSeries());
+    pieSeries.dataFields.value = "number";
+    pieSeries.dataFields.category = "type";
+    pieSeries.labels.template.disabled = true;
+    pieSeries.ticks.template.disabled = true;
+    pieSeries.slices.template.propertyFields.fill = "color";
+    pieSeries.slices.template.stroke = am4core.color("#313a46");
+    pieSeries.slices.template.strokeWidth = 1;
+    pieSeries.slices.template.strokeOpacity = 1;
+    this.pieChart = chart;
+  }
 }
